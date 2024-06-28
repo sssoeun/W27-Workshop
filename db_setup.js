358dfbaacc5aef3cc48dab7a2ee55ac278425ff8
ezard <kg1588@naver.com>
2024-06-28 11:37:02 +0900

    mysql2

--- db_setup.js
+++ db_setup.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv').config();
 const {MongoClient} = require("mongodb"); 
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 let mongodb;
 let mysqldb;
