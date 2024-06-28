b3b6f608568d7a8c13530a4d88671de7c72298f0
ezard <kg1588@naver.com>
2024-06-28 13:23:44 +0900

    html path

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 const https = require("https");
 const session = require("express-session");
 
-
 const app = express();
+const path = require('path');
 
 const options = {
     key: fs.readFileSync("./server.key"),
@@ -15,6 +15,7 @@ const options = {
 app.get('/', (req,res) => {
     res.render('index.ejs');
 });
+
 
 
 app.use(session({
@@ -29,3 +30,5 @@ https.createServer(options, app).listen(process.env.WEB_PORT, async () => {
     await setup();
     console.log(`${process.env.WEB_PORT} 포트 https 서버 실행 중.. `);
 });
+
+app.use('/', express.static(path.join(__dirname, '/')));
